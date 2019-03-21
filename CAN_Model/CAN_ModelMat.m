6828241708f89cbaaa0c805fe4cdf8d53d4c4a2e
emiltom95 <emiliano.tomaro@gmail.com>
2019-03-21 01:01:53 +0100

    CAN_Model_v5: CAN traffic tested with several bus loads and nodes

---
+++ CAN_Model/CAN_ModelMat.m
@@ -0,0 +1,25 @@
+
+clc
+clear
+
+FrameLenght = 111;
+SimTime = 0.1;
+CLK = 0.000002;
+
+NodeTiming = 0.01;
+time = NodeTiming / CLK;
+
+ID = [1,0,0,0,0,1,1,0,0,1,0];
+
+NodesID = [ 0,0,1,1,0,1,1,0,0,1,1
+            0,0,1,1,0,1,1,0,0,1,0
+            0,0,1,0,0,1,1,0,1,1,0
+            0,0,1,1,1,1,1,0,0,1,0];
+
+BusLoadInserted = 85;
+
+NumOfFrame_Max = SimTime / (CLK * FrameLenght);
+
+NumOfFrames = BusLoadInserted / 100 * NumOfFrame_Max;
+RTS_Inc = round(SimTime / CLK / NumOfFrames);
+
