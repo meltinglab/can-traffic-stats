26d00bb9a968be583612e9e9dbb5d18056e3838c
max186 <max_186@hotmail.it>
2019-03-22 15:25:44 +0100

    CAN_Model_v6 bug fixed

--- CAN_Model/CAN_ModelMat.m
+++ CAN_Model/CAN_ModelMat.m
@@ -3,8 +3,8 @@ clc
 clear
 
 FrameLenght = 110;
-SimTime = 0.1;
-CLK = 0.000002;
+SimTime = 40;
+CLK = 0.2;
 
 NodeTiming = 0.01;
 time = NodeTiming / CLK;
@@ -17,10 +17,11 @@ NodesID = [ 0,0,1,1,0,1,1,0,0,1,1
             0,0,1,1,1,1,1,0,0,1,0];
         
 
-BusLoadInserted = 90;
+BusLoadInserted = 50;
 
 NumOfFrame_Max = SimTime / (CLK * FrameLenght);
 
 NumOfFrames = BusLoadInserted / 100 * NumOfFrame_Max;
 RTS_Inc = round(SimTime / CLK / NumOfFrames);
+%RTS_Inc = 0.02/CLK;
 
